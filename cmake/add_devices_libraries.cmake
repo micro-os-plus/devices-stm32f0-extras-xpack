c971d2c1d4426ceb18be33bfbdaeb7c9fe5f3bb2
Liviu Ionescu <ilg@livius.net>
2022-02-03 10:51:05 +0200

    cmake update

---
+++ cmake/add_devices_libraries.cmake
@@ -0,0 +1,32 @@
+#
+# This file is part of the µOS++ distribution.
+#   (https://github.com/micro-os-plus)
+# Copyright (c) 2021 Liviu Ionescu
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
+#
+# -----------------------------------------------------------------------------
+
+# TODO: generate it automatically.
+add_micro_os_plus_device_stm32f0_extra_library("STM32F030x6")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F030x8")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F030xc")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F031x6")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F038xx")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F042x6")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F048xx")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F051x8")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F058xx")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F070x6")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F070xb")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F071xb")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F072xb")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F078xx")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F091xc")
+add_micro_os_plus_device_stm32f0_extra_library("STM32F098xx")
+
+# -----------------------------------------------------------------------------
